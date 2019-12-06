eb0712ec3b8bc3417c38b028e746b1c3ca424ff7
lotosbin <lotosbin@gmail.com>
2019-12-06 08:27:07 +0800

    feat(subapp): 新增的排在前面

--- src/modules/subapp/services/index.ts
+++ src/modules/subapp/services/index.ts
@@ -9,7 +9,7 @@ class SubAppService {
         try {
             const response = await fetch("http://daohang.binbinsoft.com/widget/json?name=lotosbin&pagesize=100");
             const appList = await response.json();
-            return appList.map((e: { Id: any; Name: any; Url: any; }) => ({id: e.Id, name: e.Name, url: e.Url}))
+            return appList.map((e: { Id: any; Name: any; Url: any; }) => ({id: e.Id, name: e.Name, url: e.Url})).reverse()
         } catch (e) {
             console.error(e);
             return [];
